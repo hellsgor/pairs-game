7f3e2c6c530ea707f66ac62e222aacc436618a9f
hellsgor <hellsgor@gmail.com>
2023-10-04 23:07:06 +0400

    added createContentBlock function
    - added createContentBlock function to create text blocks with heading and paragraphs
    
    (cherry picked from commit 639260ee9ee65ab05d1703d0a54f92f3135562ea)

---
+++ 09_const-var-scope/scripts/create-content-block.mjs
@@ -0,0 +1,36 @@
+import {createElement} from "./create-element.mjs";
+
+/**
+ * Функция для генерации текстового блока с заголовком и абзацами.
+ *
+ * @param {(string | string[])} blockClassName - класс который присвоится всему блоку;
+ * @param {number} titleLevel - уровень заголовка блока;
+ * @param {string} titleText - текст заголовка блока;
+ * @param {string[]} paragraphs - массив текстов абзацев. Порядок важен;
+ *
+ * @return {HTMLDivElement} - блок с заголовком и абзацами пронумерованными в классах.
+ */
+
+export function createContentBlock(
+  blockClassName,
+  titleLevel,
+  titleText,
+  paragraphs
+) {
+  const contentBlock = createElement('div', blockClassName);
+
+  const title = createElement(
+    `h${titleLevel}`,
+    `${blockClassName}-title`,
+    titleText);
+  contentBlock.append(title);
+
+  paragraphs.forEach((p, idx) => {
+    contentBlock.append(createElement(
+      'p',
+      `${blockClassName}-paragraph-${idx + 1}`,
+      p));
+  })
+
+  return contentBlock;
+}
