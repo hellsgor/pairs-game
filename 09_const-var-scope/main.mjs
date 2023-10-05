6300945b3ac928741f0a29439caf3d5845ec9142
hellsgor <hellsgor@gmail.com>
2023-10-05 16:57:12 +0400

    some fix
    (cherry picked from commit 6559d7ae1443b18bc252709dfea297218d4e7aef)

--- 09_const-var-scope/main.mjs
+++ 09_const-var-scope/main.mjs
@@ -5,15 +5,18 @@ import {prepare} from "./scripts/prepare.mjs";
 import {
   DEFAULT_PLAYING_FIELD_SIZE as fieldSize
 } from "./scripts/consts/index.js";
-import {setCardSlotSizes} from "./scripts/set-card-slot-sizes.mjs";
+import {setCardSlotSizes} from "./scripts/set-cards-size.mjs";
 
 
 const numbersArray = shuffle(createNumbersArray(fieldSize));
 
 document.addEventListener('DOMContentLoaded', () => {
   prepare(fieldSize);
-
-
+  document.getElementById('start-button')
+    .addEventListener('click', (event) => {
+      event.preventDefault();
+      console.log('start');
+    })
 })
 
 window.addEventListener('resize', () => setCardSlotSizes(fieldSize));
--- 09_const-var-scope/scripts/prepare.mjs
+++ 09_const-var-scope/scripts/prepare.mjs
@@ -1,8 +1,8 @@
 import {createElement} from "./create-element.mjs";
-import * as CONSTS from './consts/index.js';
-import {createContentBlock} from "./create-content-block.mjs";
-import {setCardSlotSizes} from "./set-card-slot-sizes.mjs";
+import {DEFAULT_PLAYING_FIELD_SIZE, HIDDEN_TITLE} from "./consts/index.js";
+import {setCardSlotSizes} from "./set-cards-size.mjs";
 import {createCards} from "./create-cards.mjs";
+import {createMenuTextContent} from './create-menu-text-content.mjs';
 
 export function prepare() {
   const body = document.querySelector('body');
@@ -10,51 +10,23 @@ export function prepare() {
   const menu = createElement('div', 'menu');
   const playingField = createElement('div', 'playing-field');
   const controls = createElement('div', 'menu__controls');
-
-  // ----- добавление контента в панель с текстом (menu) ------ начало
-  const menuTextContent = createElement('div', 'menu__text-content');
-  const menuTextStart = createContentBlock(
-    'menu__text-start',
-    3,
-    CONSTS.RULES_TEXT_START_TITLE,
-    [CONSTS.RULES_TEXT_START_TEXT],
-  );
-  const menuTextGameProgress = createContentBlock(
-    'menu__text-progress',
-    3,
-    CONSTS.RULES_TEXT_GAME_PROGRESS_TITLE,
-    [CONSTS.RULES_TEXT_GAME_PROGRESS_TEXT],
-  );
-  const menuTextEnd = createContentBlock(
-    'manu__text-end',
-    3,
-    CONSTS.RULES_TEXT_END_TITLE,
-    [CONSTS.RULES_TEXT_END_TEXT],
-  );
-  // ----- добавление контента в панель с текстом (menu) ------ конец
-
-  // ----- добавление контента в панель с текстом ------ начало
-  menuTextContent.append(createElement('p', ['menu__title', 'heading-1'], CONSTS.GAME_TITLE));
-  menuTextContent.append(createElement('p', 'menu__description', CONSTS.GAME_DESCRIPTION));
-  menuTextContent.append(menuTextStart);
-  menuTextContent.append(menuTextGameProgress);
-  menuTextContent.append(menuTextEnd);
-  // ----- добавление контента в панель с текстом ------ конец
-
+  const startButton = createElement('button', ['menu__start-button', 'button'], 'Начать игру');
+  const menuTextContent = createMenuTextContent();
 
   body.classList.add('body-background');
   body.classList.add('body-custom');
 
+  startButton.setAttribute('id', 'start-button');
 
-  menu.append(createElement('h1', 'visually-hidden', CONSTS.HIDDEN_TITLE));
+  menu.append(createElement('h1', 'visually-hidden', HIDDEN_TITLE));
   menu.append(menuTextContent);
-  menu.append(createElement('button', ['menu__start-button', 'button'], 'Начать игру'))
+  menu.append(startButton);
   menu.append(controls);
 
   container.append(menu);
   container.append(playingField);
   body.append(container);
 
-  createCards(CONSTS.DEFAULT_PLAYING_FIELD_SIZE);
+  createCards(DEFAULT_PLAYING_FIELD_SIZE);
   setCardSlotSizes();
 }
