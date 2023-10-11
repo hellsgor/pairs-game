10438cfcdf3974c4bf78f1ad5db67344ff5d10a6
hellsgor <hellsgor@gmail.com>
2023-10-11 23:26:22 +0400

    the element for selecting the size of the playing field has been improved
    (cherry picked from commit af557f68197a668cf4d00949762ea8d986729ebf)

--- 09_const-var-scope/scripts/createSizeCounter.mjs
+++ 09_const-var-scope/scripts/createSizeCounter.mjs
@@ -1,5 +1,6 @@
 import {createElement} from "./create-element.mjs";
 import {DEFAULT_PLAYING_FIELD_SIZE} from "./consts/index.js";
+import {setCustomFieldSize} from "./set-custom-field-size.mjs";
 
 export function createSizeCounter() {
   const counterWrapper = createElement('div', ['menu__size-counter', 'size-counter']);
@@ -8,6 +9,9 @@ export function createSizeCounter() {
   const increaseButton = createElement('button', ['size-counter__increase', 'button', 'button_secondary', 'button_round', 'button_small'], '+');
   const decreaseButton = createElement('button', ['size-counter__decrease', 'button', 'button_secondary', 'button_round', 'button_small'], '-');
   const counter = createElement('p', 'size-counter__counter', `${DEFAULT_PLAYING_FIELD_SIZE} x ${DEFAULT_PLAYING_FIELD_SIZE}`);
+
+  increaseButton.addEventListener('click', (event) => setCustomFieldSize(event));
+  decreaseButton.addEventListener('click', (event) => setCustomFieldSize(event));
 
   counterControls.append(decreaseButton);
   counterControls.append(counter);
