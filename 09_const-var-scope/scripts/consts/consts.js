82abf90092080259935446083fe606ceb52edb94
hellsgor <hellsgor@gmail.com>
2023-10-13 11:26:47 +0400

    add consts
    (cherry picked from commit 8a9e034e2df5a908945d4f70e0b2cbf960429a4d)

--- 09_const-var-scope/scripts/consts/consts.js
+++ 09_const-var-scope/scripts/consts/consts.js
@@ -2,6 +2,7 @@ export const GAME_TITLE = 'Пары';
 export const HIDDEN_TITLE = 'Игра в пары';
 export const GAME_DESCRIPTION = 'Вам предстоит сыграть в простую игру для тренировки памяти - Пары.';
 
+// константы текстового контента меню
 export const RULES_TEXT_START_TITLE = 'Начало игры';
 export const RULES_TEXT_START_TEXT = 'На странице выводится поле, например, 4 × 4, из карточек. Каждая карточка содержит цифру. Пользователь не видит цифры — карточки расположены рубашкой вверх. На поле строго по две карточки с одинаковой цифрой, чтобы они могли образовать пару. Карточки расположены в случайном порядке.';
 export const RULES_TEXT_GAME_PROGRESS_TITLE = 'Ход игры';
@@ -9,14 +10,20 @@ export const RULES_TEXT_GAME_PROGRESS_TEXT = 'Игрок может нажать
 export const RULES_TEXT_END_TITLE = 'Конец игры';
 export const RULES_TEXT_END_TEXT = 'Как только игрок открыл все пары на поле, игра считается завершённой.';
 
+// константы размера игрового поля
 export const DEFAULT_PLAYING_FIELD_SIZE = 4;
 export const MIN_PLAYING_FIELD_SIZE = 2;
 export const MAX_PLAYING_FIELD_SIZE = 6;
 
+// elements IDs
 export const FIELD_SIZE_COUNTER_ID = 'field-size-counter';
 export const FIELD_SIZE_COUNTER_INCREASE_BUTTON_ID = 'field-size-counter-increase-button';
 export const FIELD_SIZE_COUNTER_DECREASE_BUTTON_ID = 'field-size-counter-decrease-button';
 export const PLAYING_FIELD_ID = 'playing-field';
+export const START_BUTTON_ID = 'start-button';
+export const START_MENU_ID = 'start-menu';
+export const MENU_CONTROLS_ID = 'menu-controls';
 
+// errors texts
 export const ODD_FIELD_SIZE_ERROR_TEXT = 'Размер стороны поля должен быть чётным';
 export const ARRAY_LENGTH_DO_NOT_MATCH = ' Длинна массива слотов и длинна массива чисел не совпадают';
