995052e1b756218892f071e4777a1ef8c0930765
hellsgor <hellsgor@gmail.com>
2023-10-13 11:28:42 +0400

    функция createCard в процессе
    (cherry picked from commit b59059b2cec297e8cb411ac532cb1309a9958d35)

--- 09_const-var-scope/scripts/create-card.mjs
+++ 09_const-var-scope/scripts/create-card.mjs
@@ -1,3 +1,15 @@
+import {createElement} from "./create-element.mjs";
+
 export function createCard(slot, number) {
-  console.log(slot, number);
+  slot.append(createElement(
+    'div',
+    ['card__exactly-card', 'card__exactly-card_back']
+  ));
+  slot.append(createElement(
+    'div',
+    ['card__exactly-card', 'card__exactly-card_front', 'card__exactly-card_hidden']
+  ));
+  slot.addEventListener('click', () => {
+    console.log('click to card');
+  })
 }
