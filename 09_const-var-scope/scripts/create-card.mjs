53f2cf3582f66c8b377bc9ff5461884e0ef8a051
hellsgor <hellsgor@gmail.com>
2023-10-17 18:16:20 +0400

    vip gameMove function
    (cherry picked from commit bf08c70930920f79fecf64778e27db8565db2233)

--- 09_const-var-scope/scripts/create-card.mjs
+++ 09_const-var-scope/scripts/create-card.mjs
@@ -1,15 +1,17 @@
 import {createElement} from "./create-element.mjs";
+import {gameMove} from "./game-move.mjs";
 
 export function createCard(slot, number) {
+  const numberElement = createElement('span', 'card__number', number);
+  const cardFront = createElement(
+    'div',
+    ['card__exactly-card', 'card__exactly-card_front', 'card__exactly-card_hidden']
+  );
+  cardFront.append(numberElement);
   slot.append(createElement(
     'div',
     ['card__exactly-card', 'card__exactly-card_back']
   ));
-  slot.append(createElement(
-    'div',
-    ['card__exactly-card', 'card__exactly-card_front', 'card__exactly-card_hidden']
-  ));
-  slot.addEventListener('click', () => {
-    console.log('click to card');
-  })
+  slot.append(cardFront);
+  slot.addEventListener('click', (event) => gameMove(event));
 }
---
+++ 09_const-var-scope/scripts/game-move.mjs
@@ -0,0 +1,68 @@
+export function gameMove(event) {
+  event.preventDefault();
+  console.log(event.target);
+
+  const allFronts = Array.from(document.getElementsByClassName('card__exactly-card_front'));
+  const visibleFronts = allFronts
+    .filter((cardFront) =>
+      !cardFront.classList.contains('card__slot_guessed'))
+    .filter((cardFront) =>
+      !cardFront.classList.contains('card__exactly-card_hidden'));
+
+  console.log('before', visibleFronts);
+
+  if (visibleFronts.length < 2) {
+    visibleFronts.push(event.target.closest('.card__slot').querySelector('.card__exactly-card_front'));
+  }
+
+  console.log('after', visibleFronts);
+
+  visibleFronts.forEach((front) => {
+    if (front.classList.contains('card__exactly-card_hidden')) {
+      front.classList.remove('card__exactly-card_hidden');
+
+      const slot = front.closest('.card__slot');
+      slot.classList.add('card__slot_not-react');
+      slot.classList.add('card__slot_compare');
+
+      slot.querySelector('.card__exactly-card_back')
+        .classList.add('card__exactly-card_hidden');
+    }
+  })
+
+  if (visibleFronts.length === 2) {
+    allFronts.forEach((front) =>
+      front.closest('.card__slot').classList.add('card__slot_not-react')
+    )
+
+    const firstCardNumber = +visibleFronts[0].querySelector('.card__number').textContent;
+    const secondCardNumber = +visibleFronts[1].querySelector('.card__number').textContent;
+
+    if (firstCardNumber === secondCardNumber) {
+      visibleFronts.forEach((front) => {
+        front.classList.add('card__slot_guessed');
+      })
+    }
+
+    window.setTimeout(() => {
+      if (allFronts.every((front) => front.classList.contains('card__slot_guessed'))) {
+        console.log('you win');
+      } else {
+        allFronts.forEach((front) => {
+          const slot = front.closest('.card__slot');
+          slot.classList.remove('card__slot_compare');
+
+          if (!front.classList.contains('card__slot_guessed')) {
+            front.classList.add('card__exactly-card_hidden');
+            slot.classList.remove('card__slot_not-react');
+            slot.querySelector('.card__exactly-card_back')
+              .classList.remove('card__exactly-card_hidden');
+          }
+        })
+      }
+
+    }, 2000);
+
+
+  }
+}
