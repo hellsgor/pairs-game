fcf0501af5047fe5677d0b46560237ebc456a6ae
hellsgor <hellsgor@gmail.com>
2023-10-18 12:17:07 +0400

    рефакторинг функции gameMove
    (cherry picked from commit 9c253d149e9758ca43228631c241cc8099c531f1)

--- 09_const-var-scope/scripts/game-move.mjs
+++ 09_const-var-scope/scripts/game-move.mjs
@@ -1,67 +1,57 @@
 export function gameMove(event) {
   event.preventDefault();
 
-  const allFronts = Array.from(
-    document.getElementsByClassName('card__exactly-card_front'));
-  const visibleFronts = allFronts
-    .filter((cardFront) =>
-      !cardFront.classList.contains('card__slot_guessed'))
-    .filter((cardFront) =>
-      !cardFront.classList.contains('card__exactly-card_hidden'));
+  event.target.closest('.card__slot').dataset.compared = 'true';
 
-  if (visibleFronts.length < 2) {
-    visibleFronts.push(event.target.closest('.card__slot')
-      .querySelector('.card__exactly-card_front'));
-  }
+  const comparedCards = document
+    .querySelectorAll('.card__slot[data-compared = true]');
 
-  visibleFronts.forEach((front) => {
-    if (front.classList.contains('card__exactly-card_hidden')) {
-      front.classList.remove('card__exactly-card_hidden');
-
-      const slot = front.closest('.card__slot');
-      slot.classList.add('card__slot_rotate');
-      slot.classList.add('card__slot_not-react');
-      slot.classList.add('card__slot_compare');
-
-      slot.querySelector('.card__exactly-card_back')
-        .classList.add('card__exactly-card_hidden');
+  comparedCards.forEach((card) => {
+    if (!card.classList.contains('card__slot_rotate')) {
+      card.classList.add('card__slot_rotate');
+    }
+    if (!card.classList.contains('card__slot_compare')) {
+      card.classList.add('card__slot_compare');
     }
   })
+  
+  if (comparedCards.length === 2) {
 
-  if (visibleFronts.length === 2) {
-    allFronts.forEach((front) =>
-      front.closest('.card__slot').classList.add('card__slot_not-react')
-    )
-
-    const firstCardNumber = +visibleFronts[0]
+    const allSlots = Array.from(
+      document.getElementsByClassName('card__slot'));
+    const firstNumber = +comparedCards[0]
       .querySelector('.card__number').textContent;
-    const secondCardNumber = +visibleFronts[1]
+    const secondNumber = +comparedCards[1]
       .querySelector('.card__number').textContent;
 
-    if (firstCardNumber === secondCardNumber) {
-      visibleFronts.forEach((front) => {
-        front.classList.add('card__slot_guessed');
-      })
+    allSlots.forEach((slot) => {
+      slot.classList.add('card__slot_not-react');
+    })
+
+    if (firstNumber === secondNumber) {
+      comparedCards.forEach(((compareCard) => {
+        compareCard.classList.add('card__slot_guessed');
+      }))
     }
 
     window.setTimeout(() => {
-      if (allFronts.every((front) =>
-        front.classList.contains('card__slot_guessed'))) {
+      if (allSlots.every((slot) =>
+        slot.classList.contains('card__slot_guessed'))) {
         console.log('you win');
       }
 
-      allFronts.forEach((front) => {
-        const slot = front.closest('.card__slot');
+      allSlots.forEach((slot) => {
         slot.classList.remove('card__slot_compare');
 
-        if (!front.classList.contains('card__slot_guessed')) {
-          front.classList.add('card__exactly-card_hidden');
+        if (!(slot.classList.contains('card__slot_guessed'))) {
           slot.classList.remove('card__slot_rotate');
           slot.classList.remove('card__slot_not-react');
-          slot.querySelector('.card__exactly-card_back')
-            .classList.remove('card__exactly-card_hidden');
         }
       })
     }, 1500);
+
+    comparedCards.forEach((comparedCard) => {
+      delete comparedCard.dataset.compared;
+    })
   }
 }
