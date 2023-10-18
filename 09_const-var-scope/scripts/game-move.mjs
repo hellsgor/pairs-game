5fc5b591e539ff258caf7fd8b37fc378c9bd3ff4
hellsgor <hellsgor@gmail.com>
2023-10-18 09:18:44 +0400

    vip gameMove function - доработана
    (cherry picked from commit e035ecf0f8ef94019a1d7798654f7a4a78bedfee)

--- 09_const-var-scope/scripts/game-move.mjs
+++ 09_const-var-scope/scripts/game-move.mjs
@@ -1,21 +1,18 @@
 export function gameMove(event) {
   event.preventDefault();
-  console.log(event.target);
 
-  const allFronts = Array.from(document.getElementsByClassName('card__exactly-card_front'));
+  const allFronts = Array.from(
+    document.getElementsByClassName('card__exactly-card_front'));
   const visibleFronts = allFronts
     .filter((cardFront) =>
       !cardFront.classList.contains('card__slot_guessed'))
     .filter((cardFront) =>
       !cardFront.classList.contains('card__exactly-card_hidden'));
 
-  console.log('before', visibleFronts);
-
   if (visibleFronts.length < 2) {
-    visibleFronts.push(event.target.closest('.card__slot').querySelector('.card__exactly-card_front'));
+    visibleFronts.push(event.target.closest('.card__slot')
+      .querySelector('.card__exactly-card_front'));
   }
-
-  console.log('after', visibleFronts);
 
   visibleFronts.forEach((front) => {
     if (front.classList.contains('card__exactly-card_hidden')) {
@@ -35,8 +32,10 @@ export function gameMove(event) {
       front.closest('.card__slot').classList.add('card__slot_not-react')
     )
 
-    const firstCardNumber = +visibleFronts[0].querySelector('.card__number').textContent;
-    const secondCardNumber = +visibleFronts[1].querySelector('.card__number').textContent;
+    const firstCardNumber = +visibleFronts[0]
+      .querySelector('.card__number').textContent;
+    const secondCardNumber = +visibleFronts[1]
+      .querySelector('.card__number').textContent;
 
     if (firstCardNumber === secondCardNumber) {
       visibleFronts.forEach((front) => {
@@ -45,24 +44,22 @@ export function gameMove(event) {
     }
 
     window.setTimeout(() => {
-      if (allFronts.every((front) => front.classList.contains('card__slot_guessed'))) {
+      if (allFronts.every((front) =>
+        front.classList.contains('card__slot_guessed'))) {
         console.log('you win');
-      } else {
-        allFronts.forEach((front) => {
-          const slot = front.closest('.card__slot');
-          slot.classList.remove('card__slot_compare');
-
-          if (!front.classList.contains('card__slot_guessed')) {
-            front.classList.add('card__exactly-card_hidden');
-            slot.classList.remove('card__slot_not-react');
-            slot.querySelector('.card__exactly-card_back')
-              .classList.remove('card__exactly-card_hidden');
-          }
-        })
       }
 
-    }, 2000);
+      allFronts.forEach((front) => {
+        const slot = front.closest('.card__slot');
+        slot.classList.remove('card__slot_compare');
 
-
+        if (!front.classList.contains('card__slot_guessed')) {
+          front.classList.add('card__exactly-card_hidden');
+          slot.classList.remove('card__slot_not-react');
+          slot.querySelector('.card__exactly-card_back')
+            .classList.remove('card__exactly-card_hidden');
+        }
+      })
+    }, 1500);
   }
 }
